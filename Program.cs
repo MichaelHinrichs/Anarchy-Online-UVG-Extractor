988ece5dbf735a60c19607cbac6f3cfbb352936e
MichaelHinrichs <mchinrichs@gmail.com>
2024-03-30 12:09:16 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Anarchy_Online_UVG_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]);
+            StreamReader uvgi = new(File.OpenRead(path + ".uvgi"));
+            BinaryReader uvga = new(File.OpenRead(path + ".uvga"));
+            int fileCount = int.Parse(uvgi.ReadLine());
+
+            Directory.CreateDirectory(path);
+            for (int i = 0; i < fileCount; i++)
+            {
+                var file = uvgi.ReadLine().Split(' ');
+                uvga.BaseStream.Position = int.Parse(file[1]);
+                BinaryWriter bw = new(File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//" + file[0] + ".png"));
+                bw.Write(uvga.ReadBytes(int.Parse(file[2])));
+                bw.Close();
+            }
+        }
+    }
+}
